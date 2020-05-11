2a7f3cb8988aee6d9818ab6bc618ca2974345aca
Bui Trung Tu <tudefttry@gmail.com>
2020-05-12 00:15:38 +0700

    Khoi tao

---
+++ Website_BuyFood/Models/MonAn.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Website_BuyFood.Models
+{
+    public class MonAn
+    {
+        public int MaMon { get; set; }
+        public string TenMon { get; set; }
+        public int DonGia { get; set; }
+        public string LinkAnh { get; set; }
+        
+    }
+}
